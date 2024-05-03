5c271316e589cc15f817cbbba78e3950c79ea8fd
khanfarheenn <farrheenkhan1995@gmail.com>
2024-05-03 14:34:59 +0530

    code-refector

--- student/serializers.py
+++ student/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from authentication.models import User
-from director.models import Role
+from director.models import Role,ClassPeriod
 from .models import GuardianType, Student
 
 from django.db import IntegrityError
@@ -15,49 +15,49 @@ class GuardianTypeSerializer(serializers.ModelSerializer):
 
 class StudentSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(max_length=100, write_only=True)
-    middle_name = serializers.CharField(max_length=100, write_only=True)
+    middle_name = serializers.CharField(max_length=100, write_only=True, required=False)
     last_name = serializers.CharField(max_length=100, write_only=True)
     password = serializers.CharField(max_length=100, write_only=True)
     email = serializers.EmailField(write_only=True)
-    classes = serializers.PrimaryKeyRelatedField(queryset='director.ClassPeriod'.objects.all(), many=True)
+    classes = serializers.PrimaryKeyRelatedField(queryset=ClassPeriod.objects.all(), many=True)
 
     
 
+
     def create(self, validated_data):
-        def validate_email(self, value):
-        
-            # Check if the email already exists in the User table.
-            
-            if User.objects.filter(email=value).exists():
-                raise serializers.ValidationError("Email already exists.")
-            return value
         user_data = {
             'first_name': validated_data.pop('first_name'),
-            'middle_name': validated_data.pop('middle_name'),
+            'middle_name': validated_data.pop('middle_name', None),
             'last_name': validated_data.pop('last_name'),
             'password': validated_data.pop('password'),
             'email': validated_data.pop('email'),
         }
         classes_data = validated_data.pop('classes', [])
-
+        
+        
         try:
-            user = User.objects.get(email=user_data['email'])
-            if not user.role.filter(name='student').exists():
-                role, created = Role.objects.get_or_create(name='student')
-                if created:
-                    user.role.add(role)
-                    user.save()
-        except User.DoesNotExist:
-            user = User.objects.create_user(**user_data)
-            role = Role.objects.create(name='student')
-            user.role.add(role)
+            role, created = Role.objects.get_or_create(name='student')
         except MultipleObjectsReturned:
-            raise serializers.ValidationError("Multiple roles found for the user.")
+            raise serializers.ValidationError("Something went wrong")
+     
+        user = User.objects.filter(email=user_data['email']).first()
+
+        if  user:
+            if not user.role.filter(name='student').exists():
+                user.role.add(role)
+                user.save()
+            else:
+                raise serializers.ValidationError("User with this email already exists and is a student.")
+            
+        else:
+            user = User.objects.create_user(**user_data)
+            user.role.add(role)
+            user.save()
 
         student = Student.objects.create(user=user, **validated_data)
         student.classes.set(classes_data)
-        return student
-
+        return student  
+      
     def update(self, instance, validated_data):
         user_data = validated_data.pop('user', {})
         email = user_data.get('email')
@@ -69,7 +69,8 @@ class StudentSerializer(serializers.ModelSerializer):
             instance.user.first_name = validated_data.get('first_name', instance.user.first_name)
             instance.user.middle_name = validated_data.get('middle_name', instance.user.middle_name)
             instance.user.last_name = validated_data.get('last_name', instance.user.last_name)
-            instance.user.password = validated_data.get('password', instance.user.password)
+         
+            instance.user.set_password(validated_data.get('password')) 
             instance.user.email = validated_data.get('email', instance.user.email)
             instance.date_of_birth = validated_data.get('date_of_birth', instance.date_of_birth)
             instance.gender = validated_data.get('gender', instance.gender)
@@ -87,6 +88,7 @@ class StudentSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         representation.update({
+          
             'first_name': instance.user.first_name,
             'middle_name': instance.user.middle_name,
             'last_name': instance.user.last_name,
@@ -100,5 +102,4 @@ class StudentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Student
-        fields = ['first_name', 'middle_name', 'last_name', 'password', 'email', 'classes', 'date_of_birth', 'gender', 'enrolment_date']
-
+        exclude = ['user']
--- student/urls.py
+++ student/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import *
+
+# Define the router for the StudentView
+router = DefaultRouter()
+router.register(r'students', StudentView)
 
 
 urlpatterns = [
     path("guardian-types/", GuardianTypeView, name="guardian-type"),
     path("guardian-type/<int:pk>/", GuardianTypeView, name="guardian-type-details"),
+      path('', include(router.urls)), 
 ]
