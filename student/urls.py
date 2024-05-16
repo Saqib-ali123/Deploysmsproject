bc5fc89d6fa7ccd6e646790108851d2ac4a8c9ea
khanfarheenn <farrheenkhan1995@gmail.com>
2024-05-16 13:37:50 +0530

    refactorUrlsFile

--- student/urls.py
+++ student/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
 
-# Define the router for the StudentView
 router = DefaultRouter()
 router.register(r'students', StudentView)
 
