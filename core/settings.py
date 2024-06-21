3e9b9c69c7c8fded7a8163c2695c3736bd295f0e
Anas Irfan <anasirfan502@gmail.com>
2024-06-21 11:51:02 +0530

    Update_forgot

--- core/settings.py
+++ core/settings.py
@@ -139,7 +139,12 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+#         'LOCATION': 'unique-snowflake',
+#     }
+# }
 
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
