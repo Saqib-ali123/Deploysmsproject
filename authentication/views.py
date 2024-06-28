1eff33967f6dcfafba5359abba649e979a1b25f8
Anas Irfan <anasirfan502@gmail.com>
2024-06-28 14:55:28 +0530

    new update

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,12 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import User
 from .serializers import *
 from rest_framework import status
 from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
+
 
 
 
@@ -51,6 +53,8 @@ def UserView(request, pk=None):
 
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
+
 def ChangePasswordView(request): #ChangePasswoordView
     current_password=request.data.get('current_password')
     Change_Password=request.data.get('change_password')
