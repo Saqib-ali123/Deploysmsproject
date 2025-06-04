25da29075668bac0574327bafbaf1e0e2694f201
Noore Nabi <noorenabi845304@gmail.com>
2025-06-04 12:05:46 +0530

    update addenence model

--- attendance/models.py
+++ attendance/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from director.models import *
+from teacher.models import *
 
 
 class AttendanceSession(models.Model):
     date = models.DateField()
-    class_period = models.ForeignKey(ClassPeriod, on_delete=models.CASCADE)
-    term = models.ForeignKey(Term, on_delete=models.CASCADE)
+    year_level=models.ForeignKey(YearLevel,on_delete=models.CASCADE)
 
     class Meta:
-        unique_together = ('date', 'class_period')
+        unique_together = ('date', 'year_level')
 
     def __str__(self):
-        return f"{self.date} - {self.class_period}"
+        return f"{self.date} - {self.year_level}"
     
     
 class StudentAttendance(models.Model):
@@ -21,12 +21,13 @@ class StudentAttendance(models.Model):
         ('L', 'Leave'),
     ]
 
-    period = models.ForeignKey(AttendanceSession, on_delete=models.CASCADE, related_name="attendances")
+    session_id = models.ForeignKey(AttendanceSession, on_delete=models.CASCADE, related_name="attendances")
     student = models.ForeignKey(StudentYearLevel, on_delete=models.CASCADE)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     marked_at = models.DateField()
+    teacher=models.ForeignKey(Teacher,on_delete=models.CASCADE)
 
     class Meta:
-        unique_together = ('period', 'student')
+        unique_together = ('session_id', 'student')
     def __str__(self):
         return f"{self.student} -{self.status}"
